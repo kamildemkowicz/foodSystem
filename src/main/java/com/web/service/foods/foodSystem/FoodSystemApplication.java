a9071cd98a15750139788809c4b616ea54689862
kdemkowi <kamil.demkowicz95@gmail.com>
2018-08-15 18:54:27 +0200

    Initial commit for create food system project

---
+++ src/main/java/com/web/service/foods/foodSystem/FoodSystemApplication.java
@@ -0,0 +1,12 @@
+package com.web.service.foods.foodSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodSystemApplication.class, args);
+	}
+}
